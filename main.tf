292bedbeffeccb564b9d16ade127a21cf78adb6c
gfteix <gabriel.fteixeira13@gmail.com>
2025-10-19 12:58:56 -0300

    feat: add vpc and subnets

--- main.tf
+++ main.tf
@@ -13,6 +13,46 @@ provider "aws" {
 }
 
 
+
+// VPC: Virtual Private Network -> a fence around a bunch of resources, it separates resources from other VPCs.
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/26"
+
+  tags = {
+    Name = "new-vpc"
+  }
+}
+
+// subnet -> subdivions of the ip ranges -> a postal code
+// it is necessary to have a subnet to launch resources in a vpc
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.0.0/27"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "private-sunet"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.0.32/27"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "public-sunet"
+  }
+}
+// security group -> like a virtual firewall
+
+
+
+// route tables -> 
+// to allow internet acess to our subnet we need an internet gateway
+
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -27,6 +67,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.ec2_instance_type
+  subnet_id     = aws_subnet.private_subnet.id
 
   tags = {
     Name = var.instance_name
